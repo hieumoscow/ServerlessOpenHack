efca7c32e1d1da32fe7c6f0add463a14e385fa73
Swetha Sundar <swetha.sundr@gmail.com>
2018-09-11 14:16:09 +0800

    Add GetRating function

---
+++ models/Rating.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Oteam15
+{
+    public class Rating
+    {
+        public Guid id { get; set; }
+        public Guid userId { get; set; }
+        public Guid productId { get; set; }
+        public DateTime timestamp { get; set; }
+        public string locationName { get; set; }
+        public int rating { get; set; }
+        public string userNotes { get; set; }
+    }
+}
